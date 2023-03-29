f98da6ea1f1ca91416cae0b676e0b1b784888f6b
Antônia Turcatto <lucasturcatto.va048@academico.ifsul.edu.br>
2023-03-29 16:52:53 -0300

    1.2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,11 +8,31 @@ desses números.
  */
     println("Digite o 1° numero: ")
     var n1 = entrada.nextInt()
-    println("Valor informado: $n1")
+    //println("Valor informado: $n1")
     println("Digite o 2° numero: ")
     var n2 = entrada.nextInt()
-    println("Valor informado: $n2")
+    //println("Valor informado: $n2")
     var soma : Int = n1+n2
     println("Soma: $soma")
 
+/*
+2 - Faça um programa que calcule o quadrado de
+um número informado pelo usuário.
+ */
+    println("Informe um numero: ")
+    var numQuadrado : Float = entrada.nextFloat()
+    numQuadrado *= numQuadrado
+    entrada.nextLine()
+    println("O quadrado do numero informado é $numQuadrado")
+
+/*
+3 - Faça um programa que peça ao usuário para informar seu nome e sua idade, e que
+exiba uma mensagem de boas-vindas com o nome e a idade informados.
+ */
+
+    println("Informe o seu nome: ")
+    var nome : String = entrada.nextLine()
+    println("Sua idade: ")
+    var idade : Int = entrada.nextInt()
+    println("Bem vinde!\n$nome, $idade anos.")
 }
